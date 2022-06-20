3afa17de327a68ee9f3871fc88bdb4124cb24fca
Dhyani Desai <65896239+dhyani21@users.noreply.github.com>
2022-06-20 19:45:47 +0530

    Added solution - LeetHub

--- First Repeating Element - GFG/first-repeating-element.py
+++ First Repeating Element - GFG/first-repeating-element.py
@@ -1,17 +1,16 @@
 #User function Template for python3
-
+from collections import Counter
 class Solution:
     #Function to return the position of the first repeating element.
     def firstRepeated(self,arr, n):
         
         #arr : given array
         #n : size of the array
-        sets = set(arr)
-        if len(sets) == len(arr):
-            return -1
-        for val in arr:
-            if(arr.count(val) > 1):
-                return arr.index(val) + 1
+        m=Counter(arr)
+        for i in arr:
+            if m[i]>1:
+                return arr.index(i)+1
+        return -1
 
 
 
